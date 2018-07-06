e9659d01b1eac3a1be145d8c79f878062a55b6a4
wl <965053280@qq.com>
2018-07-07 00:41:32 +0800

    第二次提交

--- git_test44/src/cn/itcast/domain.java
+++ git_test44/src/cn/itcast/domain.java
@@ -2,6 +2,9 @@ package cn.itcast;
 
 public class domain {
     public static void main(String[] args) {
+
         System.out.println("hello git");
+        System.out.println("hello git1");
+        System.out.println("hello git2");
     }
 }
